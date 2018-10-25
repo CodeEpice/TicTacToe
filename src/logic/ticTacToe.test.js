2a5be0825acb016a5ca23219c1a2b40b82efd3be
gudrunivans <gudruni17@ru.is>
2018-10-25 16:11:10 +0000

    basic functionality of tic tac toe board

--- src/logic/ticTacToe.test.js
+++ src/logic/ticTacToe.test.js
@@ -1,6 +1,9 @@
 // ticTacToe.test.js
 
-const playerSwap = require("./ticTacToe")
+const playerSwap = require("./ticTacToe");
+const board = require("./board");
+
+let x = new board(3,3);
 
 test("Should return the given symbol", () => {
 	expect(playerSwap("x")).toBe("o");
@@ -8,4 +11,12 @@ test("Should return the given symbol", () => {
 
 test("Should return the given symbol", () => {
 	expect(playerSwap("o")).toBe("x");
+});
+
+test("Should return true if value could be set", () => {
+	expect(x.setValue(4,"x")).toBe(true);
+});
+
+test("Should return true if value could be set", () => {
+	expect(x.getValue(4)).toBe("x");
 });
